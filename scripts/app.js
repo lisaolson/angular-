88ab1c5572534008788754373c5c2713d4ef4476
Lisa Olson <olson.lisa94@gmail.com>
2016-09-22 15:11:54 -0700

    Styling changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,22 @@
 console.log('app.js is linked!');
 
-angular.module('musicApp', [])
-  .controller('musicController', musicController);
+angular
+  .module('musicApp', [])
 
-musicController.$inject = ['$http'];
-  function musicController($http){
-    console.log(musicController);
-    var vm = this;
-        $http({
-          method: 'GET',
-          url: 'https://api.soundcloud.com/tracks/?client_id=9a5b10b365e2b82fef5ece0ce57d6e03'
-        }).then(function successCallback(response) {
-          console.log(response.data);
-          vm.tracks = response.data;
-        }, function errorCallback(response) {
-          console.log('Error getting data', response);
-        });
-    }
-
-
-
-    //
-    // config.$inject = ['$routeProvider', '$locationProvider'];
-    // function config ( $routeProvider, $locationProvider ) {
-    //   $routeProvider
-    //     .when('/', {
-    //       templateUrl: 'templates/music.html',
-    //       controller: 'MusicIndexController',
-    //       controllerAs: 'musicIndexCtrl'
-    //     })
-    //     .otherwise({
-    //       redirectTo: '/'
-    //     });
-    //
-    //     $locationProvider
-    //     .html5Mode({
-    //       enabled: true,
-    //       requireBase: false
-    //     });
-    // }
+// angular.module('musicApp', [])
+//   .controller('musicController', musicController);
+//
+// musicController.$inject = ['$http'];
+//   function musicController($http){
+//     console.log(musicController);
+//     var vm = this;
+//         $http({
+//           method: 'GET',
+//           url: 'https://api.soundcloud.com/tracks/?client_id=9a5b10b365e2b82fef5ece0ce57d6e03'
+//         }).then(function successCallback(response) {
+//           console.log(response.data);
+//           vm.tracks = response.data;
+//         }, function errorCallback(response) {
+//           console.log('Error getting data', response);
+//         });
+//     }
