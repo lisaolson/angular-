856f69385ba95718fa4abf2a211b5e8644a94ef7
Lisa Olson <olson.lisa94@gmail.com>
2016-09-22 11:57:57 -0700

    Cut down to app.js and index.html file. It's rendering data on screen from SoundCloud API

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,41 @@
 console.log('app.js is linked!');
 
+angular.module('musicApp', [])
+  .controller('musicController', musicController);
 
-angular.module('musicApp', ['ngResource']);
-angular.module('musicApp').factory('Music', function($resource){
-  return $resource('https://api.soundcloud.com/tracks/?client_id=9a5b10b365e2b82fef5ece0ce57d6e03');
-});
-angular.module('musicApp').controller('musicIndexController', [function() {
-  var vm = this;
-  vm.music = [];
+musicController.$inject = ['$http'];
+  function musicController($http){
+    console.log(musicController);
+    var vm = this;
+        $http({
+          method: 'GET',
+          url: 'https://api.soundcloud.com/tracks/?client_id=9a5b10b365e2b82fef5ece0ce57d6e03'
+        }).then(function successCallback(response) {
+          console.log(response.data);
+          vm.tracks = response.data;
+        }, function errorCallback(response) {
+          console.log('Error getting data', response);
+        });
+    }
 
-  this.getMusic = function() {
-    vm.music = Music.query();
-  }
 
-}]);
-// angular.module('musicApp', ['ngRoute', 'ngResource'])
-//     .config(config);
-//
-//     config.$inject = ['$routeProvider', '$locationProvider'];
-//     function config ( $routeProvider, $locationProvider ) {
-//       $routeProvider
-//         .when('/', {
-//           templateUrl: 'templates/music.html',
-//           controller: 'MusicIndexController',
-//           controllerAs: 'musicIndexCtrl'
-//         })
-//         .otherwise({
-//           redirectTo: '/'
-//         });
-//
-//         $locationProvider
-//         .html5Mode({
-//           enabled: true,
-//           requireBase: false
-//         });
-//     }
+
+    //
+    // config.$inject = ['$routeProvider', '$locationProvider'];
+    // function config ( $routeProvider, $locationProvider ) {
+    //   $routeProvider
+    //     .when('/', {
+    //       templateUrl: 'templates/music.html',
+    //       controller: 'MusicIndexController',
+    //       controllerAs: 'musicIndexCtrl'
+    //     })
+    //     .otherwise({
+    //       redirectTo: '/'
+    //     });
+    //
+    //     $locationProvider
+    //     .html5Mode({
+    //       enabled: true,
+    //       requireBase: false
+    //     });
+    // }
